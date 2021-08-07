55c63416197b35397c4ca610876b27ef62a8d75b
Manasi Paradkar <paradkar.manasi@gmail.com>
2021-08-07 17:38:53 +0000

    Add Instructions of how to play game

--- run.py
+++ run.py
@@ -1,1 +1,14 @@
 # write code
+
+def instructions():
+    """
+    Function to display the instructions on how to play
+    """
+    print("Welcome!")
+    print("Instructions to play the game:")
+    print("1. Enter row and column number to select a cell, Example \"2 3\"")
+    print("2. In order to flag a mine: Enter F after row and column numbers, Example \"2 3 F\"")
+    print("3. If you step on a mine, then GAME OVER")
+
+instructions()
+
